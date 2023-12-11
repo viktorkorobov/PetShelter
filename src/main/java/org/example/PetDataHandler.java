7ba06d5558ba1eaaaa306de550ab48494a075f66
viktorkorobov <viktorkorobov2012@gmail.com>
2023-12-11 21:12:37 +0200

    Homework16-2

---
+++ src/main/java/org/example/PetDataHandler.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+public class PetDataHandler {
+    private static final String FILE_PATH = "petData.json";
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    static {
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+    }
+
+    public static List<Pet> loadPetData() {
+        try {
+            return objectMapper.readValue(new File(FILE_PATH),
+                    objectMapper.getTypeFactory().constructCollectionType(List.class, Pet.class));
+        } catch (IOException e) {
+            // обробка винятків при неможливості завантаження даних
+            return List.of();
+        }
+    }
+
+    public static void savePetData(List<Pet> pets) {
+        try {
+            objectMapper.writeValue(new File(FILE_PATH), pets);
+        } catch (IOException e) {
+            // обробка винятків при неможливості збереження даних
+            e.printStackTrace();
+        }
+    }
+}
