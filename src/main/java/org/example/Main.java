1eb69c76a2eacb70894fcd5afa5d5b50ec50decd
viktorkorobov <40566605+viktorkorobov@users.noreply.github.com>
2023-12-11 21:14:32 +0200

    Delete src/main/java/org/example/Main.java

--- src/main/java/org/example/Main.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Main extends PetShelterApp{
-    public static void main(String[] args){
-        PetShelterApp petShelterApp = new PetShelterApp();
-        petShelterApp.run();
-    }
-}
